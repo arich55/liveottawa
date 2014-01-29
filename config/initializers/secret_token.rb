b9b62f23fc82b62274c50244cb0a88d8d524d1a2
Andre Richards <andrev.richards@gmail.com>
2014-01-28 19:51:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Liveottawa::Application.config.secret_key_base = 'a8904497c61b271cd660e95c816b0e455278718c0c0ccf0db3503c0841f6a95e5e09b07fc5420e409e8c5292ba08ca84e076646c55d02756b555d6ecef15a3b1'
